5a0125ce0f7dad8a8b230931e18e5de49ffd9da7
Jocy Gonçalves Neto <jocygneto@gmail.com>
2023-08-25 00:21:58 +0000

    Parametrização para oq eu o componente pegue os cards de maneira dinâmica.

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
 import React from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
+
+import MovieCard from "./MovieCard";
 
 import "./App.css";
 import searchIcon from "./search.svg";
 
 const API_URL = `https://www.omdbapi.com/?apikey=4caf39f5`;
 
-const movieTest = {
-  Title: "Spiderman in Cannes",
-  Year: "2016",
-  imdbID: "tt5978586",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BZDlmMGQwYmItNTNmOS00OTNkLTkxNTYtNDM3ZWVlMWUyZDIzXkEyXkFqcGdeQXVyMTA5Mzk5Mw@@._V1_SX300.jpg",
-};
-
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
 
-    console.log(data.Search);
+    setMovies(data.Search);
   };
 
   //Como se fosse o ngOninit
   useEffect(() => {
-    searchMovies("spiderman");
+    searchMovies("The Lord of the Rings");
   }, []); //[] ~~> Utilizado vazio para chamar apenas no start da pagina
 
   return (
@@ -35,35 +31,29 @@ function App() {
       <div className="search">
         <input
           placeholder="Search for movies"
-          value="Superman"
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={searchIcon} alt="Search" onClick={() => {}} />
+        <img
+          src={searchIcon}
+          alt="Search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
 
-      <div className="container">
-        <div className="movie">
-          <div>
-            <p>{movieTest.Year}</p>
-          </div>
+      {movies?.length > 0 ? (
+        <div className="container">
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
 
-          <div>
-            <img
-              src={
-                movieTest.Poster !== "N/A"
-                  ? movieTest.Poster
-                  : "https://via.placeholder.com/400"
-              }
-              alt={movieTest.Title}
-            />
-          </div>
-
-          <div>
-            <span>{movieTest.Type}</span>
-            <h3>{movieTest.Title}</h3>
-          </div>
+          {/*  */}
         </div>
-      </div>
+      ) : (
+        <div className="empty">
+          <h2>No movies found</h2>
+        </div>
+      )}
     </div>
   );
 }
